db0c1c4a581ccf0340e9a7a28bf9fed3502c8a4a
XiaoLiang <liujian594@hotmail.com>
2016-11-08 17:43:59 +0800

    20161108

--- app/src/main/java/xiaoliang/ltool/activity/note/NoteAddActivity.java
+++ app/src/main/java/xiaoliang/ltool/activity/note/NoteAddActivity.java
@@ -1,14 +1,29 @@
 package xiaoliang.ltool.activity.note;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import xiaoliang.ltool.R;
+import xiaoliang.ltool.activity.LToolApplication;
 
 public class NoteAddActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //时间，地址，金额等固定部分
+    private View addressLayout,moneyLayout,timeLayout;
+    private TextInputEditText addressEditText,moneyEditText,advanceEditText;
+    private View addressCancel,moneyCancel,timeCancel;
+    private RadioGroup incomeGroup;
+    private TextView startDate,startTime,endDate,endTime;
+    //勾选项，内容项，列表项
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,7 @@ public class NoteAddActivity extends AppCompatActivity implements View.OnClickLi
         setSupportActionBar(toolbar);
         if(getSupportActionBar()!=null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 
--- app/src/main/java/xiaoliang/ltool/view/note/NoteAddItem.java
+++ app/src/main/java/xiaoliang/ltool/view/note/NoteAddItem.java
@@ -1,48 +1,66 @@
 package xiaoliang.ltool.view.note;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.support.design.widget.TextInputEditText;
-import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import xiaoliang.ltool.R;
-import xiaoliang.ltool.util.DensityUtil;
 
 /**
  * Created by liuj on 2016/11/5.
  * 记事本添加的item
  */
 
-public class NoteAddItem extends LinearLayout implements TextInputEditText.OnFocusChangeListener {
+public class NoteAddItem extends LinearLayout implements
+        TextInputEditText.OnFocusChangeListener,
+        View.OnClickListener,
+        CompoundButton.OnCheckedChangeListener,
+        RadioGroup.OnCheckedChangeListener{
     private Context context;
-    private LinearLayout root;
-    private LinearLayout title;
+    private CheckBox checkBox;
+    private TextView number;
     private TextInputEditText editText;
     private ImageView cancel;
-    private int type = 0;
-    private int index = 0;
-    private boolean isChecked = false;
+    private TextView endTime,startTime,endDate,startDate;
+    private TextInputEditText advanceNum;
+    private AppCompatSpinner advanceUnit;
+    private SwitchCompat allDay,alert;
+    private RadioGroup radioGroup;
+    private int startYear,startMonth,startDay,startHour,startMinute,endYear,endMonth,endDay,endHour,endMinute;
+    private int type = -1;
+    private int index = -1;
+//    private boolean isChecked = false;
+    private int income = 0;
+    private OnNoteAddItemClickListener clickListener;
     public static final int LIST = 0;
     public static final int TODO = 1;
     public static final int TEXT = 2;
     public static final int MONEY = 3;
     public static final int TIME = 4;
     public static final int ADDRESS = 5;
+    private String[] advanceUnitNames = {"分钟","小时"," 天 "," 周 "};
 
     public NoteAddItem(Context context,int type) {
-        this(context);
-        setType(type);
+        super(context);
+        init(type);
     }
 
     public NoteAddItem(Context context) {
@@ -56,95 +74,229 @@ public class NoteAddItem extends LinearLayout implements TextInputEditText.OnFoc
     public NoteAddItem(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
-        init();
+        init(TEXT);
     }
-    private void init() {
+    private void init(int t) {
 //        LayoutInflater.from(context).inflate(R.layout.item_note_add_check,
 //                this, true);
-//        root = (LinearLayout) findViewById(R.id.item_note_add_root);
-//        title = (LinearLayout) findViewById(R.id.item_note_add_title);
-//        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
-//        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
-//        editText.setOnFocusChangeListener(this);
-//        setType(type);
-    }
-
-    public void setType(int type) {
-        if(this.type!=type){
-            this.type = type;
-            switch (type){
-                case LIST:
-                    setListType();
-                    break;
-                case TODO:
-                    setTodoType();
-                    break;
-                case TEXT:
-                    break;
-                case MONEY:
-                    break;
-                case TIME:
-                    break;
-                case ADDRESS:
-                    break;
-            }
+        if(type==t)
+            return;
+        type = t;
+        switch (type){
+            case LIST:
+                setListType();
+                break;
+            case TODO:
+                setTodoType();
+                break;
+            case TEXT:
+                setTextType();
+                break;
+            case MONEY:
+                setMoneyType();
+                break;
+            case TIME:
+                setTimeType();
+                break;
+            case ADDRESS:
+                setAddressType();
+                break;
         }
     }
+
     private void setTodoType(){
-        CheckBox checkBox = new CheckBox(context);
-        checkBox.setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                NoteAddItem.this.isChecked = isChecked;
-                if(isChecked){
-                    editText.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG| Paint.ANTI_ALIAS_FLAG);  // 设置中划线并加清晰
-                }else{
-                    editText.getPaint().setFlags(0);  // 取消设置的的划线
-                }
-            }
-        });
-        title.removeAllViews();
-        title.addView(checkBox);
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_check,
+                this, true);
+        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
+        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
+        checkBox = (CheckBox) findViewById(R.id.item_note_add_checkbox);
+        editText.setOnFocusChangeListener(this);
+        cancel.setOnClickListener(this);
     }
 
     private void setListType(){
-        TextView textView = new TextView(context);
-        textView.setGravity(Gravity.CENTER);
-        textView.setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        textView.setTextSize(DensityUtil.dip2px(context,18));
-        //将字体文件保存在assets/fonts/目录下，创建Typeface对象
-        Typeface typeFace = Typeface.createFromAsset(context.getAssets(), "fonts/century_gothic_std.TTF");
-        //使用字体
-        textView.setTypeface(typeFace);
-        textView.setTextColor(Color.GRAY);
-        title.removeAllViews();
-        title.addView(textView);
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_list,
+                this, true);
+        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
+        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
+        number = (TextView) findViewById(R.id.item_note_add_number);
+        editText.setOnFocusChangeListener(this);
+        cancel.setOnClickListener(this);
+    }
+
+    private void setTextType(){
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_check,
+                this, true);
+        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
+        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
+        editText.setOnFocusChangeListener(this);
+        cancel.setOnClickListener(this);
+    }
+
+    private void setMoneyType(){
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_money,
+                this, true);
+        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
+        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
+        radioGroup = (RadioGroup) findViewById(R.id.item_note_add_income);
+//        editText.setOnFocusChangeListener(this);
+        cancel.setOnClickListener(this);
+        radioGroup.setOnCheckedChangeListener(this);
+    }
+
+    private void setTimeType(){
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_time,
+                this, true);
+        endTime = (TextView) findViewById(R.id.item_note_add_end_time);
+        startTime = (TextView) findViewById(R.id.item_note_add_start_time);
+        endDate = (TextView) findViewById(R.id.item_note_add_end_date);
+        startDate = (TextView) findViewById(R.id.item_note_add_start_date);
+        advanceNum = (TextInputEditText) findViewById(R.id.item_note_add_advance_num);
+        advanceUnit = (AppCompatSpinner) findViewById(R.id.item_note_add_advance_unit);
+        allDay = (SwitchCompat) findViewById(R.id.item_note_add_day);
+        alert = (SwitchCompat) findViewById(R.id.item_note_add_alert);
+        advanceUnit.setAdapter(new ArrayAdapter<>(context,R.layout.item_note_add_time_spinner,R.id.item_note_add_time_spinner_text,advanceUnitNames));
+        endTime.setOnClickListener(this);
+        startTime.setOnClickListener(this);
+        endDate.setOnClickListener(this);
+        startDate.setOnClickListener(this);
+        Calendar calendar = Calendar.getInstance();
+        startYear = calendar.get(Calendar.YEAR);
+        startMonth = calendar.get(Calendar.MONTH);
+        startDay = calendar.get(Calendar.DAY_OF_MONTH);
+        startHour = calendar.get(Calendar.HOUR_OF_DAY);
+        startMinute = calendar.get(Calendar.MINUTE);
+        calendar.set(Calendar.MINUTE,startMinute+15);
+        endYear = calendar.get(Calendar.YEAR);
+        endMonth = calendar.get(Calendar.MONTH);
+        endDay = calendar.get(Calendar.DAY_OF_MONTH);
+        endHour = calendar.get(Calendar.HOUR_OF_DAY);
+        endMinute = calendar.get(Calendar.MINUTE);
+        setDate();
+    }
+
+    private void setAddressType(){
+        LayoutInflater.from(context).inflate(R.layout.item_note_add_address,
+                this, true);
+        editText = (TextInputEditText) findViewById(R.id.item_note_add_text);
+        cancel = (ImageView) findViewById(R.id.item_note_add_cancel);
+        cancel.setOnClickListener(this);
+    }
+
+    private void setDate(){
+        endDate.setText(endYear+"年"+endMonth+"月"+endDay+"日");
+        startDate.setText(startYear+"年"+startMonth+"月"+startDay+"日");
+        endTime.setText(endHour+":"+endMinute);
+        startTime.setText(startHour+":"+startMinute);
+    }
+
+    public void setType(int type) {
+        init(type);
     }
 
     public void setIndex(int index,int maxSize) {
         if(this.index != index&&type == LIST){
             this.index = index;
-            View v = title.getChildAt(0);
-            if(v instanceof TextView){
-                String text = index+"";
-                String max = maxSize+"";
-                while(text.length()<max.length()){
-                    text = "0"+text;
-                }
-                ((TextView)v).setText(index+"");
+            if(number==null)
+                return;
+            String text = index+"";
+            String max = maxSize+"";
+            while(text.length()<max.length()){
+                text = "0"+text;
             }
+            number.setText(text);
         }
     }
 
     public void setChecked(boolean isChecked){
-        if(this.isChecked != isChecked&&type == TODO){
-            this.isChecked = isChecked;
-            View v = title.getChildAt(0);
-            if(v instanceof CheckBox){
-                ((CheckBox)v).setChecked(isChecked);
-            }
+        if(type == TODO&&checkBox!=null){
+            checkBox.setChecked(isChecked);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.item_note_add_cancel:
+                if(clickListener!=null)
+                    clickListener.onClickCancelBtn(this);
+                break;
+            case R.id.item_note_add_end_date:
+                new DatePickerDialog(context,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                endYear = year;
+                                endMonth = monthOfYear;
+                                endDay = dayOfMonth;
+                                setDate();
+                            }
+                        },endYear,endMonth,endDay).show();
+                break;
+            case R.id.item_note_add_end_time:
+                new TimePickerDialog(context,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                endHour = hourOfDay;
+                                endMinute = minute;
+                                setDate();
+                            }
+                        },endHour,endMinute,true).show();
+                break;
+            case R.id.item_note_add_start_date:
+                new DatePickerDialog(context,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                startYear = year;
+                                startMonth = monthOfYear;
+                                startDay = dayOfMonth;
+                                setDate();
+                            }
+                        },startYear,startMonth,startDay).show();
+                break;
+            case R.id.item_note_add_start_time:
+                new TimePickerDialog(context,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                startHour = hourOfDay;
+                                startMinute = minute;
+                                setDate();
+                            }
+                        },startHour,startMinute,true).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.item_note_add_checkbox:
+                if(isChecked){
+                    editText.getPaint().setFlags(Paint. STRIKE_THRU_TEXT_FLAG|Paint.ANTI_ALIAS_FLAG);  // 设置中划线并加清晰
+                }else{
+                    editText.getPaint().setFlags(0);  // 取消设置的的划线
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.item_note_add_income_in:
+                income = 0;
+                break;
+            case R.id.item_note_add_income_out:
+                income = 1;
+                break;
+        }
+    }
+
+    public interface OnNoteAddItemClickListener{
+        void onClickCancelBtn(View v);
     }
 
     @Override
@@ -155,5 +307,9 @@ public class NoteAddItem extends LinearLayout implements TextInputEditText.OnFoc
             else
                 cancel.setVisibility(View.GONE);
         }
+    }
+
+    public void setClickListener(OnNoteAddItemClickListener clickListener) {
+        this.clickListener = clickListener;
     }
 }
