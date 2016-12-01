686d5d3012b37c578f395c1d3848e500c0d5d13b
XiaoLiang <liujian594@hotmail.com>
2016-12-01 08:32:53 +0800

    20161130

---
+++ app/src/main/java/xiaoliang/ltool/constant/TranslationConstant.java
@@ -0,0 +1,12 @@
+package xiaoliang.ltool.constant;
+
+/**
+ * Created by Liuj on 2016/11/30.
+ * 翻译用的秘钥库
+ */
+
+public class TranslationConstant {
+    public static final String BAIDU_APPID = "2015063000000001";
+    public static final String BAIDU_KEY = "12345678";
+
+}
